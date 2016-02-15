87c169e8763fb81b86908e8e3a35f389a70c4ec4
Kiran Akkineni <kiran.home@gmail.com>
2016-02-15 15:49:09 -0800

    big changes

--- slack-app.js
+++ slack-app.js
@@ -76,8 +76,8 @@ controller.on('create_bot', function(bot,config) {
         if (err) {
           console.log(err);
         } else {
-          convo.say('kopychat joined your team');
-          convo.say('/invite kopychat to channels in which you plan to use it!');
+          convo.say('I am the knote, just joined your team');
+          convo.say('You must now /invite me to a channel so that I can be of use!');
         }
       });
 
@@ -107,7 +107,7 @@ controller.hears('','direct_message,direct_mention,mention',function(bot, messag
 
   bot.api.users.info({'user': message.user}, function(err, response) {
     data.app_user_name  = response.user.name;
-    kopy(data);
+    saveKnote(data);
   });
 
   bot.reply(message,'Your message has been saved. Thank you.')
@@ -116,7 +116,7 @@ controller.hears('','direct_message,direct_mention,mention',function(bot, messag
 controller.on('slash_command', function(bot,message) {
   var data = {};
 
-  data.user_id          = message.user_id;
+  data.user_id          = message. user_id;
   data.message          = message.text;
   data.app_name         = 'slack';
   data.app_user_name    = message.user_name;
@@ -124,36 +124,36 @@ controller.on('slash_command', function(bot,message) {
 
 
   bot.replyPublic(message, 'Your message has been saved. Thank you.');
-  kopy(data);
+  saveKnote(data);
 });
 
 
-function kopy(data) {
+function saveKnote(data) {
 
-  var queryString = util.format("INSERT INTO messages (user_id,message,app_name,app_user_name,app_group_name) VALUES ('%s','%s','%s','%s','%s')",
+  var querySrring = util.format("INSERT INTO knote (user_id,message,app_name,app_user_name,app_group_name) VALUES ('%s','%s','%s','%s','%s')",
                                  data.user_id,
                                  data.message,
                                  data.app_name,
                                  data.app_user_name,
                                  data.app_group_name);
 
-    executeQuery(queryString);
+    exequteQuery(querySrring);
 }
 
 function dbMigrate() {
     console.log('Migration started ...');
 
-    var query = "CREATE TABLE IF NOT EXISTS messages (ID bigserial PRIMARY KEY, " +
+    var query = "CREATE TABLE IF NOT EXISTS knote (ID bigserial PRIMARY KEY, " +
                 "user_id VARCHAR(200) null, message TEXT null, app_name VARCHAR(100) null," +
                 "app_user_name VARCHAR(100) null, app_group_name VARCHAR(100) null, " +
                 "create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW())";
 
-    executeQuery(query);
+    exequteQuery(query);
     console.log('Migration Ended');
 
 }
 //DB query function
-function executeQuery(queryString) {
+function exequteQuery(querySrring) {
     // Get a Postgres client from the connection pool
     pg.connect(conString, function(err, client, done) {
         // Handle conString errors
@@ -164,7 +164,7 @@ function executeQuery(queryString) {
         }
 
         // SQL Query > Insert Data
-        var query = client.query(queryString);
+        var query = client.query(querySrring);
 
         // Stream results back one row at a time
         query.on('row', function(row) {
