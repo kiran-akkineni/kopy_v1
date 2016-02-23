a2d4b959cf8e8e1ebc5abd3521c53e062487de27
Eftakhairul Islam <eftakhairul@gmail.com>
2016-02-23 11:58:52 -0500

    Some changes in slack app.

--- slack-app.js
+++ slack-app.js
@@ -16,15 +16,15 @@ var hangoutsBot       = require('hangouts-bot');
 //Inputs from Slack
 Config.clientId       = process.env.CLIENT_ID     || '19936248482.21489538647';
 Config.clientSecret   = process.env.CLIENT_SECRET || 'c71c603cf8fce0c4d840ad9ca794c9c3';
+Config.redirect_uri   = process.env.REDIRECT_URI  || 'https://kopychat.heroku.com';
 
 //Database inputs
 Config.port           = process.env.PORT          || 5000;
 Config.conString      = process.env.DATABASE_URL  || 'postgres://vagrant@localhost:5432/vagrant';
-Config.redirect_uri   = process.env.REDIRECT_URI  || 'https://kopychat.heroku.com';
 
 //hangouts
 Config.username       = process.env.USERNAME      || 'kopychat@gmail.com';
-Config.password       = process.env.PGPASSWORD    || 'slashkopy0';
+Config.password       = process.env.PASSWORD      || 'slashkopy0';
 
 
 
