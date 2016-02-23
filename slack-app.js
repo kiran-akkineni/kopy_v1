2ae08c0c1ec39233f2e2a41586890fb7123b5b04
Eftakhairul Islam <eftakhairul@gmail.com>
2016-02-23 01:54:48 -0500

    indentation is updated

--- slack-app.js
+++ slack-app.js
@@ -5,31 +5,31 @@
 "use strict";
 
 // required libraries
-global.base_path    = __dirname;
-global.Config       = {};
+global.base_path      = __dirname;
+global.Config         = {};
 
-global.ModuleLoader = require(base_path + '/src/library/moduleLoader.js');
+global.ModuleLoader   = require(base_path + '/src/library/moduleLoader.js');
 
-var Botkit          = require('botkit');
-var hangoutsBot     = require("hangouts-bot");
+var Botkit            = require('botkit');
+var hangoutsBot       = require("hangouts-bot");
 
 //Inputs from Slack
-Config.clientId      = process.env.CLIENT_ID     || '19936248482.21489538647';
-Config.clientSecret  = process.env.CLIENT_SECRET || 'c71c603cf8fce0c4d840ad9ca794c9c3';
+Config.clientId       = process.env.CLIENT_ID     || '19936248482.21489538647';
+Config.clientSecret   = process.env.CLIENT_SECRET || 'c71c603cf8fce0c4d840ad9ca794c9c3';
 
 //Database inputs
-Config.port          = process.env.PORT          || 5000;
-Config.conString     = process.env.DATABASE_URL  || 'postgres://vagrant@localhost:5432/vagrant';
-Config.redirect_uri  = process.env.REDIRECT_URI  || 'https://kopychat.heroku.com';
+Config.port           = process.env.PORT          || 5000;
+Config.conString      = process.env.DATABASE_URL  || 'postgres://vagrant@localhost:5432/vagrant';
+Config.redirect_uri   = process.env.REDIRECT_URI  || 'https://kopychat.heroku.com';
 
-Config.username      = process.env.USERNAME      || "kopychat@gmail.com";
-Config.password      = process.env.PGPASSWORD    || "slashkopy0";
+Config.username       = process.env.USERNAME      || "kopychat@gmail.com";
+Config.password       = process.env.PGPASSWORD    || "slashkopy0";
 
 
 
-var slackbot    = ModuleLoader.bot('slackbot')(Botkit);
-var hangoutbot  = ModuleLoader.bot('hangoutsbot')(hangoutsBot);
+var slackbot          = ModuleLoader.bot('slackbot')(Botkit);
+var hangoutbot        = ModuleLoader.bot('hangoutsbot')(hangoutsBot);
 
 //Calling function to create database tables
-var slackModel       = ModuleLoader.model('slack');
+var slackModel        = ModuleLoader.model('slack');
 slackModel.migrate();
