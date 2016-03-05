a87292fd2370340b2f485b3148085b2331d2406d
Eftakhairul Islam <eftakhairul@gmail.com>
2016-03-05 13:03:02 -0500

    logged in done

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,19 @@
 import {bootstrap}    from 'angular2/platform/browser'
+import {bind, provide} from 'angular2/core';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {AppComponent} from './app.component'
 
-bootstrap(AppComponent);
+
+import {HTTP_PROVIDERS, Http} from 'angular2/http';
+import {AuthHttp, AuthConfig} from 'angular2-jwt';
+
+bootstrap(AppComponent, [
+          HTTP_PROVIDERS,
+          ROUTER_PROVIDERS,
+          bind(LocationStrategy).toClass(HashLocationStrategy),
+          provide(AuthConfig, { useFactory: () => {
+              return new AuthConfig();
+          }}),
+          AuthHttp
+]);
+
