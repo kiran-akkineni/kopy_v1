c4e02a7f29065f86445370382f42fd636848b458
Eftakhairul Islam <eftakhairul@gmail.com>
2016-03-11 17:19:30 -0500

    cross site domain support added

--- server/bots/slackbot.js
+++ server/bots/slackbot.js
@@ -47,6 +47,21 @@ module.exports =  function(Botkit)  {
      webserver.use(cookieParser());
      webserver.use(express.static('./public'));
 
+     webserver.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token, Authorization');
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        // Pass to next layer of middleware
+        next();
+    });
+
       webserver.get('/message', function(req,res) {
           slackModel.find(function(result) {
             res.json(result);
