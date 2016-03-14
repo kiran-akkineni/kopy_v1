a92c09fb719f8192788d5b3ebfad1f4c0fa638f1
Eftakhairul Islam <eftakhairul@gmail.com>
2016-03-14 18:26:47 -0400

    done

--- app/app.setting.js
+++ app/app.setting.js
@@ -1,4 +1,6 @@
-System.register([], function(exports_1) {
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var AppSettings;
     return {
         setters:[],
@@ -7,12 +9,12 @@ System.register([], function(exports_1) {
                 function AppSettings() {
                 }
                 Object.defineProperty(AppSettings, "API_ENDPOINT", {
-                    get: function () { return 'https://kopychat.heroku.com'; },
+                    get: function () { return 'https://kopychat.herokuapp.com'; },
                     enumerable: true,
                     configurable: true
                 });
                 return AppSettings;
-            })();
+            }());
             exports_1("AppSettings", AppSettings);
         }
     }
