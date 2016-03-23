ea1229f5521e0b32a08c468c4ec081181d6cb52c
Eftakhairul Islam <eftakhairul@gmail.com>
2016-03-23 04:19:58 -0400

    url pointing to heroku

--- app/app.setting.ts
+++ app/app.setting.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
-   public static get API_ENDPOINT(): string { return 'http://localhost:5000'; }
-   //public static get API_ENDPOINT(): string { return 'https://kopychat.herokuapp.com'; }
+   //public static get API_ENDPOINT(): string { return 'http://localhost:5000'; }
+   public static get API_ENDPOINT(): string { return 'https://kopychat.herokuapp.com'; }
    public static get AUTH_CLIRNT_ID(): string { return 'kWSNpHIdfmhITKDVYu9dZ5UB57sQZ6JQ'; }
    public static get AUTH_APP_URL(): string { return 'app47501252.auth0.com'; }
 }
