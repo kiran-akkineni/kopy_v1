f13723f638b2ba56445727fe4a8f3d8b4190baf4
Eftakhairul Islam <eftakhairul@gmail.com>
2016-04-05 09:41:46 -0400

    slack bot is updated

---
+++ server/controllers/user.js
@@ -0,0 +1,72 @@
+/**
+ * @author Eftakhairul Islam <eftakhairul@gmail.com>
+ */
+
+'use strict';
+
+var UserController = {};
+var userModel      = ModuleLoader.model('user');
+var userNoteModel  = ModuleLoader.model('user_note');
+    
+UserController.post = function (req, res) {
+    var data          = {};
+    data.name         = req.body.name;
+    data.email        = req.body.email;
+
+    var auth          = req.body.user_id.split("|");
+
+    data.auth_type    = auth[0];
+    data.auth_user_id = (auth.length > 1)? auth[1]:'';
+
+
+    userModel.find({email: data.email.trim().toString()}, function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            if (result.length > 0) {
+                res.json({status: 'user already exist.'});
+            } else {
+              userModel(data).save(function (err) {
+                   if (err) throw err;
+                    res.json({status: 'okay'});
+                   });
+            }
+          }
+        });
+    
+};
+
+UserController.createUserNoteMap = function (req, res) {
+    var map              = {};
+    map.email            = req.body.email;
+    map.app_group_name   = req.body.app_group_name;
+    map.app_user_name    = req.body.app_user_name;
+
+    userNoteModel.findByEmail(map.email, function (results) {
+
+      if (results.length > 0) {
+          console.log('mapping is already exist');
+      } else {
+        userNoteModel(map).save(function () {
+          console.log('new map created.');
+        });
+      }
+    });
+
+    res.json({status: 'okay'});
+};
+
+UserController.getUserNoteMap = function (req, res) {
+
+    userNoteModel.findByEmail(req.query.email, function (results) {
+      if (results.length > 0) {
+          res.json(results);
+      } else {
+          res.json([]);
+      }
+    });
+};
+
+var exports = module.exports  = UserController;
+
+
