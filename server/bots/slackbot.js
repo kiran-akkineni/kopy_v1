8780614204c1aad551df5271ba869974d4440030
Eftakhairul Islam <eftakhairul@gmail.com>
2016-05-14 05:12:41 -0400

    fb message is done

--- server/bots/slackbot.js
+++ server/bots/slackbot.js
@@ -78,9 +78,10 @@ module.exports =  function(Botkit)  {
                 client.post('v2.6/me/messages?access_token=' + Config.page_token, jsonData, function(err, res, body) {
                   lastText =  message.message.text;
                   console.log('response sent successfully. Status: ' + res.statusCode);
-                  res.status(200).send();
               });
           }
+
+          res.sendStatus(200);
       });
 
 
