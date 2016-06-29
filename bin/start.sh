1d6fe2f2f9ebeeab8c5f739502a1ea88de252360
Eftakhairul Islam <eftakhairul@gmail.com>
2016-06-29 01:09:47 -0400

    scripts folder renamed to bin folder

---
+++ bin/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+REDIRECT_URI=https://f5b4b206.ngrok.io  CLIENT_ID=3578811873.21294826611 CLIENT_SECRET=ddd5742d580c9d3ea01f17f6253f5a47 node server/slack-app.js
--- scripts/start.sh
+++
@@ -1,1 +0,0 @@
-REDIRECT_URI=https://4d9d74eb.ngrok.io  CLIENT_ID=3578811873.21294826611 CLIENT_SECRET=ddd5742d580c9d3ea01f17f6253f5a47 node server/slack-app.js
