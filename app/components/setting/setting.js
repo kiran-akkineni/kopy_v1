dd3f48ea84c7fca3dda4b6eb0917d9d819e193ef
Eftakhairul Islam <eftakhairul@gmail.com>
2016-08-17 02:58:40 -0400

    change password is done

--- app/components/setting/setting.js
+++ app/components/setting/setting.js
@@ -32,21 +32,59 @@ System.register(['angular2/core', 'angular2/router', '../../services/authcheckse
             function (_1) {}],
         execute: function() {
             Setting = (function () {
-                function Setting(fromBuilder, profileService) {
+                function Setting(fromBuilder, profileService, ref) {
                     this.fromBuilder = fromBuilder;
                     this.profileService = profileService;
+                    this.ref = ref;
+                    this.flashMessage = { "mgs": "",
+                        "type": "success" };
                 }
                 Setting.prototype.ngOnInit = function () {
                     var _this = this;
-                    this.usernameFrom = this.fromBuilder.group({ username: ["", common_1.Validators.required] });
+                    this.usernameFrom = this.fromBuilder.group({ username: ["", common_1.Validators.compose([common_1.Validators.required, common_1.Validators.minLength(4)])] });
+                    this.passwordChangeFrom = this.fromBuilder.group({
+                        old_password: ["", common_1.Validators.required],
+                        password: ["", common_1.Validators.compose([common_1.Validators.required, common_1.Validators.minLength(4)])],
+                        confirmPassword: ["", common_1.Validators.compose([common_1.Validators.required, common_1.Validators.minLength(4)])]
+                    }, { validator: this.mismatchedPasswords('password', 'confirmPassword') });
                     this.profileService.get()
                         .then(function (data) {
                         if (data && data.username)
                             _this.usernameFrom.controls.username.updateValue(data.username);
                     });
+                    this.setFlashMessage("");
                 };
-                Setting.prototype.saveUserName = function (event) {
+                Setting.prototype.saveUserName = function () {
+                    var _this = this;
+                    this.profileService.save(this.usernameFrom.value)
+                        .then(function (data) {
+                        if (data.status == false) {
+                            _this.setFlashMessage("Sorry! username already taken.", "danger");
+                        }
+                        else {
+                            _this.setFlashMessage("Username has been updated.", "success");
+                        }
+                    });
+                };
+                Setting.prototype.setFlashMessage = function (mgs, type) {
+                    if (type === void 0) { type = "success"; }
+                    this.flashMessage.mgs = mgs;
+                    this.flashMessage.type = "alert alert-" + type;
+                };
+                Setting.prototype.changePassword = function (event) {
                     event.preventDefault();
+                    alert(this.passwordChangeFrom.valid);
+                };
+                Setting.prototype.mismatchedPasswords = function (passwordKey, confirmPasswordKey) {
+                    return function (group) {
+                        var password = group.controls[passwordKey];
+                        var confirmPassword = group.controls[confirmPasswordKey];
+                        if (password.value !== confirmPassword.value) {
+                            return {
+                                mismatchedPasswords: true
+                            };
+                        }
+                    };
                 };
                 Setting = __decorate([
                     core_1.Component({
@@ -55,7 +93,7 @@ System.register(['angular2/core', 'angular2/router', '../../services/authcheckse
                         templateUrl: './components/setting/setting.html'
                     }),
                     router_1.CanActivate(function () { return authcheckservice_1.tokenNotExpired(); }), 
-                    __metadata('design:paramtypes', [common_1.FormBuilder, profileservice_1.ProfileService])
+                    __metadata('design:paramtypes', [common_1.FormBuilder, profileservice_1.ProfileService, core_1.ChangeDetectorRef])
                 ], Setting);
                 return Setting;
             }());
--- app/components/setting/setting.ts
+++ app/components/setting/setting.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit}              from 'angular2/core';
+import {Component, OnInit, ChangeDetectorRef}              from 'angular2/core';
 import {CanActivate}                    from 'angular2/router';
 import {tokenNotExpired}                from '../../services/authcheckservice';
 import {ProfileService}                 from '../../services/profileservice';
@@ -16,17 +16,63 @@ import 'rxjs/add/operator/map';
 export class Setting implements OnInit
 {
     usernameFrom:ControlGroup;
-    constructor(private fromBuilder: FormBuilder, private profileService:ProfileService) {}
+    passwordChangeFrom:ControlGroup;
+    flashMessage = {"mgs" : "",
+                    "type": "success"};
+    constructor(private fromBuilder: FormBuilder, private profileService:ProfileService, private ref: ChangeDetectorRef) {}
 
     ngOnInit() {
-        this.usernameFrom = this.fromBuilder.group({username: ["", Validators.required]});
+        this.usernameFrom = this.fromBuilder.group({username: ["", Validators.compose([Validators.required, Validators.minLength(4)])]});
+
+        this.passwordChangeFrom =  this.fromBuilder.group({
+            old_password    :       ["", Validators.required],
+            password        :       ["", Validators.compose([Validators.required, Validators.minLength(4)])],
+            confirmPassword :    ["", Validators.compose([Validators.required, Validators.minLength(4))]
+        }, {validator: this.mismatchedPasswords('password', 'confirmPassword')});
+
         this.profileService.get()
 			               .then((data) => {
 			                   if(data && data.username) this.usernameFrom.controls.username.updateValue(data.username);
 			               });
+        this.setFlashMessage("");
     }
 
-    saveUserName(event) {
-            event.preventDefault();
+    saveUserName() {
+            this.profileService.save(this.usernameFrom.value)
+                               .then((data) => {
+                                   if (data.status == false) {
+                                       this.setFlashMessage("Sorry! username already taken.","danger");
+                                   } else {
+                                        this.setFlashMessage("Username has been updated.","success");
+                                   }
+
+                               });
     }
+
+    setFlashMessage(mgs, type="success") {
+        this.flashMessage.mgs       = mgs;
+        this.flashMessage.type      = "alert alert-"+type;
+    }
+
+    changePassword(event) {
+        event.preventDefault();
+        alert(this.passwordChangeFrom.valid);
+
+
+    }
+
+    mismatchedPasswords(passwordKey: string, confirmPasswordKey: string) {
+        return (group: ControlGroup): {[key: string]: any} => {
+            let password = group.controls[passwordKey];
+            let confirmPassword = group.controls[confirmPasswordKey];
+
+            if (password.value !== confirmPassword.value) {
+              return {
+                mismatchedPasswords: true
+              };
+            }
+        }
+    }
+
+
 }
